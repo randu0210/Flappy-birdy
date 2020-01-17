4f9a30fa6d09cae8ef9acc5a03bc262122256be1
randu0210 <Scream0utgaming@gmail.com>
2020-01-17 11:32:06 +0700

    final update

---
+++ Assets/FlylittleBird.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlylittleBird : MonoBehaviour {
+public gamemanager gm;
+public float velocity = 1;
+private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(Input.GetMouseButtonDown(0))	{
+      rb.velocity = Vector2.up * velocity;
+	}
+	}
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		gm.GameOver();
+	}
+}
---
+++ Assets/gamemanager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class gamemanager : MonoBehaviour {
+
+	public GameObject gameOverCanvas;
+
+	private void Start()
+	{
+		gameOverCanvas.SetActive(false);
+		Time.timeScale=1;
+	}
+	public void GameOver()
+	{
+		gameOverCanvas.SetActive(true);
+		Time.timeScale=0;
+	}
+	public void Replay()
+	{
+		SceneManager.LoadScene(0);
+	}
+}
